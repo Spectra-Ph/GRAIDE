a10b504da323f4055c73b218129424f437e55f62
Spectra-Ph <100085970+Spectra-Ph@users.noreply.github.com>
2023-09-29 13:44:53 -0400

    Update FileForm.js

--- frontend/components/FileForm.js
+++ frontend/components/FileForm.js
@@ -1,4 +1,4 @@
-import './FileForm.css'
+/*import './FileForm.css'
 import { useState } from 'react';
 
 
@@ -54,6 +54,81 @@ function FileForm() {
           { file && <p>{file.name}</p>}
         </div>
     )
+}
+
+export default FileForm;
+*/
+import './FileForm.css';
+import { useState } from 'react';
+
+function FileForm() {
+    const [file1, setFile1] = useState(null);
+    const [file2, setFile2] = useState(null);
+
+    const handleFileInputChange = (event, setFile) => {
+        setFile(event.target.files[0]);
+    }
+
+    const handleSubmit = async (event, uploadEndpoint, file) => {
+        event.preventDefault();
+        const formData = new FormData();
+        formData.append('file_upload', file);
+
+        try {
+            const endpoint = `http://localhost:8000/${uploadEndpoint}/`; // Adjust the port if needed
+            const response = await fetch(endpoint, {
+                method: "POST",
+                body: formData
+            });
+
+            if (response.ok) {
+                console.log("File uploaded successfully");
+            } else {
+                console.error("Failed to upload file");
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    const handleGrade = async () => {
+        try {
+            const endpoint = "http://localhost:8000/grade"; // Adjust the port if needed
+            const response = await fetch(endpoint, {
+                method: "POST"
+            });
+
+            if (response.ok) {
+                console.log("Grading initiated successfully");
+            } else {
+                console.error("Failed to initiate grading");
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    return (
+        <div>
+            <h1>Upload files</h1>
+            <form onSubmit={(event) => handleSubmit(event, "uploadfile_1", file1)}>
+                <input type="file" onChange={(event) => handleFileInputChange(event, setFile1)} />
+                <button className="upl" type="submit">Upload Student's Answer</button>
+            </form>
+
+            <form onSubmit={(event) => handleSubmit(event, "uploadfile_2", file2)}>
+                <input type="file" onChange={(event) => handleFileInputChange(event, setFile2)} />
+                <button className="upl" type="submit">Upload Solution Manual</button>
+            </form>
+
+            <button className="grade" onClick={handleGrade}>Grade</button>
+
+            
+
+            {file1 && <p>{file1.name}</p>}
+            {file2 && <p>{file2.name}</p>}
+        </div>
+    );
 }
 
 export default FileForm;
