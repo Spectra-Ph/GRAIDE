2359b1d4508e21226e0584e1ccc89b6b29f53dad
Spectra-Ph <100085970+Spectra-Ph@users.noreply.github.com>
2023-09-28 23:32:54 -0400

    Create App.js

---
+++ frontend/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import FileForm from './components/FileForm';
+
+function App() {
+  return (
+    <div className="App">
+      <FileForm/>
+      
+    </div>
+  );
+}
+
+export default App;
